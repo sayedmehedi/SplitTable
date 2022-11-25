3f0487786f6593cad64c0ca8631f9e207415e24b
MohiuddinAkib <mohiuddinmostafakamal@gmail.com>
2022-11-25 20:33:55 +0600

    [fix]: fbsdk and google sigin sdk import in ios appdelegate.m

--- ios/SplitTable/AppDelegate.m
+++ ios/SplitTable/AppDelegate.m
@@ -4,6 +4,9 @@
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 
+#import <FBSDKCoreKit/FBSDKCoreKit-swift.h>
+#import <RNGoogleSignin/RNGoogleSignin.h>
+
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
 #import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
@@ -11,8 +14,7 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-#import <FBSDKCoreKit/FBSDKCoreKit-swift.h>
-#import <RNGoogleSignin/RNGoogleSignin.h>
+
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
