58e9b36d0fb65d4e4e54ce4b49a3eb6be409466f
MohiuddinAkib <mohiuddinmostafakamal@gmail.com>
2022-11-26 09:51:40 +0600

    [fix]: android locatin permission issue

--- src/hooks/useGetGeolocationQuery.ts
+++ src/hooks/useGetGeolocationQuery.ts
@@ -1,35 +1,44 @@
 import React from "react";
-import {Alert} from "react-native";
 import {useQuery} from "@tanstack/react-query";
 import {QueryKeys} from "@constants/query-keys";
+import {PermissionsAndroid, Platform} from "react-native";
 import Geolocation from "@react-native-community/geolocation";
 import {GeolocationError, GeolocationPosition} from "@src/models";
+
+function getGeolocation() {
+  return new Promise<GeolocationPosition>((resolve, reject) => {
+    try {
+      Geolocation.getCurrentPosition(
+        (position: GeolocationPosition) => {
+          console.log("getCurrentPosition success", position);
+          resolve(position);
+        },
+        (error: GeolocationError) => {
+          console.log("getCurrentPosition error", error.message);
+
+          reject(error);
+        },
+        {
+          enableHighAccuracy: true,
+        },
+      );
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
 
 function useGetGeolocationQuery() {
   return useQuery<GeolocationPosition, Error>(
     [QueryKeys.GEOLOCATION],
-    () => {
-      return new Promise<GeolocationPosition>((resolve, reject) => {
-        try {
-          // Geolocation.requestAuthorization();
-          Geolocation.getCurrentPosition(
-            (position: GeolocationPosition) => {
-              console.log("getCurrentPosition success", position);
-              resolve(position);
-            },
-            (error: GeolocationError) => {
-              console.log("getCurrentPosition error", error.message);
+    async () => {
+      if (Platform.OS === "android" && Platform.Version >= 23) {
+        await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+        );
+      }
 
-              reject(error);
-            },
-            {
-              enableHighAccuracy: true,
-            },
-          );
-        } catch (err) {
-          reject(err);
-        }
-      });
+      return await getGeolocation();
     },
     {
       networkMode: "always",
